fc7f89eb7edb21faeeae2b33f3cbd63754f3d86f
Aman Bhargava <56464156+BhargavaCreates@users.noreply.github.com>
2020-11-17 19:17:20 +0530

    no param handler

--- server.js
+++ server.js
@@ -1,53 +1,54 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 var moment = require('moment')
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// enable CORS 
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-app.get("/api/timestamp/:date",(req,res) => {
-
-  var validate = function(t){
-    console.log(t);
-    typeof(t)
-    var valid = (new Date(parseInt(t))).getTime() > 0;
-    console.log(valid)
-    return valid
-  }
-
-
-  if(validate(req.params.date)){
-    res.json({
-      'unix': new Date(parseInt(req.params.date)).getTime(),
-      'utc': new Date(parseInt(req.params.date)).toUTCString()
-    })
-  } else {
-   res.json({
-     "error" : "Invalid Date (expected Format: YYYY-MM-DD)"
-   })
-  }
+app.get('/api/timestamp',(req,res) => {
+  res.json({
+    "unix": Date.now(),
+    "utc": new Date().toUTCString()
+  })
 })
 
+app.get('/api/timestamp/:dateString',(req,res) => {
+  const dateString = req.params.dateString;
+  let date;
+
+  if(!dateString){
+    date = new Date();
+  }
+
+  if(!isNaN(dateString)){
+    date = new Date(parseInt(dateString));
+  } else {
+    date = new Date(dateString);
+  }
+
+  if(date.toString() === 'Invalid Date'){
+    res.json({
+      error: date.toString()
+    })
+  } else {
+    res.json({
+      unix: date.getTime(),
+      utc : date.toUTCString()
+    })
+  }
+
+
+
+}) 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
